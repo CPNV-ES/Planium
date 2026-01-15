c758e6aac06f290aaf9318ae7efcc6897b107f55
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-15 11:43:29 +0100

    feat(): React to location changes

--- src/components/CoordinateForm.vue
+++ src/components/CoordinateForm.vue
@@ -2,28 +2,36 @@
 import MapPreview from "@/components/mapPreview.vue";
 import {ref} from "vue";
 // Check later to use the given inputs for location
-const lat = ref(0.0)
-const lng = ref(0.0)
+const lat = ref(0)
+const lng = ref(0)
 
+const location = ref({
+  lat: undefined,
+  lng: undefined
+})
+
+function send(){
+  location.value = {lat: parseFloat(lat.value), lng: parseFloat(lng.value)}
+}
 </script>
 
 <template>
-  <div class="flex flex-row w-[70%] justify-center items-center p-6">
+  <div class="flex flex-row w-full justify-center items-center p-6">
       <div class="bg-base-100 w-full max-w-sm shrink-0">
         <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
           <legend class="fieldset-legend">Your position</legend>
 
           <label class="label">Latitude</label>
-          <input :value="lat" type="text" class="input" placeholder="42.161836383" />
+          <input v-model="lat" type="number" class="input" placeholder="42.161836383" />
 
           <label class="label">Longitude</label>
-          <input :value="lng" type="text" class="input" placeholder="6.13838333" />
+          <input v-model="lng" type="number" class="input" placeholder="6.13838333" />
 
-          <button class="btn btn-neutral mt-4">Find</button>
+          <button type="submit" @click="send()" class="btn btn-neutral mt-4">Find</button>
         </fieldset>
       </div>
-    <div id="cesiumContainer" class="flex w-full">
-       <mapPreview :lng :lat />
+    <div id="cesiumContainer" class="flex w-[70%]">
+       <mapPreview v-bind="location" />
     </div>
   </div>
 </template>
--- src/components/mapPreview.vue
+++ src/components/mapPreview.vue
@@ -1,13 +1,14 @@
 <script setup>
 import {VcViewer, VcCompass, VcNavigation, VcTerrainProviderCesium, VcLayerImagery, VcImageryProviderOsm} from "vue-cesium";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 const viewerRef = ref(null)
 const isViewerReady = ref(false)
-
-const props = defineProps({
-  lng: 46.82166054184684,
-  lat:  6.500465335539498
+const viewer = ref()
+const location = defineProps({
+  lng: undefined,
+  lat:  undefined
 })
+
 
 onMounted(() => {
   viewerRef.value.creatingPromise.then((readyObj) => {
@@ -17,10 +18,27 @@ onMounted(() => {
 })
 
 const onViewerReady = (readyObj) => {
+  flyTo(readyObj, location.lat, location.lng)
+  viewer.value = readyObj
+}
+
+watch(
+    [() => location.lat, () => location.lng],
+    ([newLat, newLng]) => {
+      if (viewer.value && newLat && newLng) {
+        flyTo(viewer.value, newLat, newLng)
+      }
+    }
+)
+
+
+function flyTo(readyObj, lat, lng){
+try {
+
   readyObj.viewer.camera.flyTo({
     destination: readyObj.Cesium.Cartesian3.fromDegrees(
-        6.500465335539498, // longitude
-        46.82166054184684, //latitude
+        lng ?? 6.500465335539498, // longitude
+        lat ?? 46.82166054184684, //latitude
         100, //height
     ),
     orientation: {
@@ -30,19 +48,20 @@ const onViewerReady = (readyObj) => {
     }
   })
 }
+catch(e) {
+  console.log(e)
+}
 
-
+}
 
 </script>
 
 <template>
-
-  <vc-viewer access-token="YOUR_ACCESS_TOKEN"
+  <vc-viewer access-token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MzYzYjQ5Yi1hYzkzLTRhODctYWNkNi04ZWRmM2VhNmU2MzEiLCJpZCI6Mzc3NTg1LCJpYXQiOjE3NjgyMDg0NjN9.-_Sbjc8qnYU-KGxD2wBzXxvDPcGkasVe2vZCEY9KtmI"
              ref="viewerRef"
              @ready="onViewerReady">
       <vc-layer-imagery>
         <vc-imagery-provider-osm>
-
         </vc-imagery-provider-osm>
       </vc-layer-imagery>
       <vc-imagery-provider-amap />
