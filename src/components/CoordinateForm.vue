ec55ade3b440d8cfd569d89a9a300c03e59233a9
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-15 09:55:51 +0100

    feat(): Change positioning and sizing

--- src/components/CoordinateForm.vue
+++ src/components/CoordinateForm.vue
@@ -4,24 +4,26 @@ import {ref} from "vue";
 // Check later to use the given inputs for location
 const lat = ref(0.0)
 const lng = ref(0.0)
+
 </script>
 
 <template>
-      <div class="px-6 text-center lg:text-left">
-        <h1 class="text-center py-2 text-5xl font-bold">Planium</h1>
+  <div class="flex flex-row w-[70%] justify-center items-center p-6">
+      <div class="bg-base-100 w-full max-w-sm shrink-0">
+        <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend class="fieldset-legend">Your position</legend>
+
+          <label class="label">Latitude</label>
+          <input :value="lat" type="text" class="input" placeholder="42.161836383" />
+
+          <label class="label">Longitude</label>
+          <input :value="lng" type="text" class="input" placeholder="6.13838333" />
+
+          <button class="btn btn-neutral mt-4">Find</button>
+        </fieldset>
       </div>
-      <div class="bg-base-100 w-full max-w-sm shrink-0 px-6">
-        <div class="">
-          <fieldset class="fieldset">
-            <label class="label">Latitude</label>
-            <input @input="lat" type="text" class="input" placeholder="0.0.0.0" />
-            <label class="label">Longitude</label>
-            <input @input="lng" type="text" class="input" placeholder="0.0.0.0" />
-            <button class="btn btn-neutral mt-4">Find</button>
-          </fieldset>
-        </div>
-      </div>
-  <div id="cesiumContainer" class="px-6 w-full">
-    <mapPreview :lng :lat />
+    <div id="cesiumContainer" class="flex w-full">
+       <mapPreview :lng :lat />
+    </div>
   </div>
 </template>
