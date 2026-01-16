e99684ae4890ac474e3e976cba4961b484ba8e5d
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-16 14:31:55 +0100

    feat(): Create component for terrain

--- src/components/terrain/mapTerrain.vue
+++ src/components/terrain/mapTerrain.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {onMounted, ref} from "vue";
+import {VcTerrainProviderCesium} from "vue-cesium";
 
 const imageryProvider = ref(null)
 const props = defineProps({
@@ -15,5 +16,5 @@ onMounted(async ()=> {
 </script>
 
 <template>
-  <vc-terrain-provider-arcgis ref="https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer"></vc-terrain-provider-arcgis>
+  <vc-terrain-provider-cesium ref="provider"></vc-terrain-provider-cesium>
 </template>
