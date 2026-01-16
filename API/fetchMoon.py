d6047c95652e0795bb30e3d93c89a8d0c8c74ca4
davidgalindo-git <>
2026-01-16 16:34:03 +0100

    refactor:modified comments and prevent params format errors

--- API/fetchMoon.py
+++ API/fetchMoon.py
@@ -16,9 +16,9 @@ def get_moon_data(start_time, stop_time, step): # src: https://ssd-api.jpl.nasa.
             'MAKE_EPHEM': 'YES',
             'EPHEM_TYPE': 'OBSERVER',
             'CENTER': "'500@399'",      # Earth's center coordinates
-            'START_TIME': f'{start_time}',   # From 'YYYY-MM-DD'
-            'STOP_TIME': f'{stop_time}',    # To 'YYYY-MM-DD'
-            'STEP_SIZE': f'{step}',        # Step (ex.'1h')
+            'START_TIME': f"'{start_time}'",   # From 'YYYY-MM-DD'
+            'STOP_TIME': f"'{stop_time}'",    # To 'YYYY-MM-DD'
+            'STEP_SIZE': f"'{step}'",        # Step (ex.'1h')
             'QUANTITIES': "'1,9'",    # 1 = RA/DEC, Quantity 9 = Brightness/Phase
         }
 
@@ -30,7 +30,7 @@ def get_moon_data(start_time, stop_time, step): # src: https://ssd-api.jpl.nasa.
 
     lines = data.get('result', '').split('\n') # Split text in lines
 
-    moon_data = [] # dic to return
+    moon_data = [] # dic with data to return
 
     is_data_zone = False # data zone flag
     for line in lines:
@@ -49,22 +49,20 @@ def get_moon_data(start_time, stop_time, step): # src: https://ssd-api.jpl.nasa.
                     values.append(v)
 
              # NASA column mapping for Quantities 1,9:
-                 # parts[0,1] = Date, Time
-                 # parts[2,3,4] = RA (h, m, s)
-                 # parts[5,6,7] = DEC (d, m, s)
-                 # parts[8] = APmag
-                 # parts[9] = S-br
-                 # parts[10] = Illu% (Phase)
+                 # values[0,1] = Date, Time
+                 # values[2,3,4] = RA (h, m, s)
+                 # values[5,6,7] = DEC (d, m, s)
+                 # values[8] = APmag
+                 # values[9] = S-br
+                 # values[10] = Illu% (Phase)
 
             if len(values) >= 10:
                 try:
                     # Convert RA (sexagesimal) to decimal
-                    ra_decimal = round((
+                    ra_decimal = (
                     float(values[2]) # h (hours)
                     + float(values[3])/60    # min to h
                     + float(values[4])/3600  # seconds to h
-                    ),
-                    6 # round to 6 decimals
                     )
 
                     # Convert DEC (sexagesimal) to decimal
@@ -74,20 +72,19 @@ def get_moon_data(start_time, stop_time, step): # src: https://ssd-api.jpl.nasa.
                     else:
                         sign=1
 
-                    dec_decimal = round((
+                    dec_decimal = (
                     abs(float(values[5]))  # d (degrees) absolute value
                     + float(values[6])/60 # arcminutes to d
                     + float(values[7])/3600 # arcseconds to d
-                    ),
-                    6 # round to 6 decimals
                     )
+
                     dec_decimal *= sign # apply sign
 
                     # structure return data
                     entry = {
                         'datetime': f"{values[0]} {values[1]}", # 'YYYY-MM-DD HH:SS'
-                        'ra': ra_decimal,   # Decimal Right Ascension (degrees)
-                        'dec': dec_decimal, # Decimal Declination (degrees)
+                        'ra': round(ra_decimal, 6),   # 6 Decimal Right Ascension (degrees)
+                        'dec': round(dec_decimal,6),  # 6 Decimal Declination (degrees)
                         'phase': float(values[-1]) # Phase decimal percentage (0.00 - 100.00)
                     }
                     moon_data.append(entry)
