08e65bb4a044416a92200c7cd399c1e327dc0ede
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-16 14:30:43 +0100

    feat():Add function to get a tilset position from any API

--- src/utils/tileset.js
+++ src/utils/tileset.js
@@ -1,3 +1,7 @@
 export async function get3dTilesetById(id) {
    return await Cesium.Cesium3DTileset.fromIonAssetId(id)
 }
+
+export async function getTilesetCartesian(){
+    return {x:-29,y: 8, z:25}
+}
