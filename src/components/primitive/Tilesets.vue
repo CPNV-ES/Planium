419d700d6939413c74af57908efe57c293704fec
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-16 10:09:45 +0100

    feat():Create a Tilesets.vue to call all tilesets components

---
+++ src/components/primitive/Tilesets.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+import Google3dTiles from "@/components/primitive/Google3dTiles.vue";
+import MoonTileset from "@/components/MoonTileset.vue";
+
+</script>
+
+<template>
+  <Google3dTiles :assetId="2275207"/>
+  <MoonTileset :assetId="2684829"/>
+</template>
