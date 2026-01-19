e06bfcdc6dd09c611294b2d2b4c9384e13493b96
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-19 08:40:05 +0100

    feat(): Make moon placed in real place

--- src/components/primitive/Tileset.vue
+++ src/components/primitive/Tileset.vue
@@ -14,18 +14,22 @@ const cesiumToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN;
       })
 
 const onReady = ({ cesiumObject: tileset }) => {
-  const tilesetPosition = new Cesium.Cartesian3(
-      props.tilesetCartesian.x,
-      props.tilesetCartesian.y,
-      props.tilesetCartesian.z
-  )
-  const cartographic = Cesium.Cartographic.fromCartesian(tilesetPosition) // put real moon place if possible
-  const surface = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, cartographic.height)
-  const offset = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, 1000)
-  const translation = Cesium.Cartesian3.subtract(offset, surface, new Cesium.Cartesian3())
-  const translationMatrix = Cesium.Matrix4.fromTranslation(translation)
+  const now = Cesium.JulianDate.now();
+  const moonPosInertial = Cesium.Simon1994PlanetaryPositions.computeMoonPositionInEarthInertialFrame(now);
+  const icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix(now);
+  const moonPositionReal = new Cesium.Cartesian3();
+  if (Cesium.defined(icrfToFixed)) {
+    Cesium.Matrix3.multiplyByVector(icrfToFixed, moonPosInertial, moonPositionReal);
+  }
+  const cartographic = Cesium.Cartographic.fromCartesian(moonPositionReal);
+  const finalPosition = Cesium.Cartesian3.fromRadians(
+      cartographic.longitude,
+      cartographic.latitude,
+      150000
+  );
+  const translationMatrix = Cesium.Matrix4.fromTranslation(finalPosition)
 
-  const scaleFactor = 0.1
+  const scaleFactor = 0.01
   const scaleMatrix = Cesium.Matrix4.fromUniformScale(scaleFactor)
   tileset.modelMatrix = Cesium.Matrix4.multiply(
       translationMatrix,
