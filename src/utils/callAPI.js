8f9ba9c1bc138fe208811c41959cf4c48c77d229
omegasofian2304 <pn82bby@eduvaud.ch>
2026-01-20 14:28:38 +0100

    docs(): Add more comments

--- src/utils/callAPI.js
+++ src/utils/callAPI.js
@@ -9,17 +9,23 @@ Desc : call the API from the frontend
 // Source : https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 export async function getData(url, lat = null, long = null) {
     try {
-        // include the parameters in the url
+        // Include the parameters in the url
         const fullURL = `${url}?lat=${lat}&long=${long}`;
+
+        // Fetch data from the API
         const response = await fetch(fullURL);
+
+        // Check if the response was successful
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
 
+        // Parse the JSON response
         const result = await response.json();
         console.log(result);
         return result;
     } catch (error) {
+        // Log the error and return null
         console.error(error.message);
         return null;
     }
