59f73f937bdf7b542845388c343910e2475f6b0c
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-20 19:08:21 +0100

    feat(): Call the move plane function

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {VcViewer} from "vue-cesium";
 import {ref, watch} from "vue";
-import {loadPlanes, prepareScene, removeMoving} from "@/utils/scene.js";
+import {loadPlanes, movePlanes, prepareScene, removeMoving} from "@/utils/scene.js";
 import Imagery from "@/components/imagery/Imagery.vue";
 import {flyTo} from "@/utils/camera.js";
 import Navigation from "@/components/navigation/Navigation.vue";
@@ -11,8 +11,8 @@ import Moon from "@/components/primitive/Moon.vue";
 const viewerRef = ref(null)
 const isViewerReady = ref(false)
 const cesiumToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN;
-const Vcviewer = ref()
-const cesium = ref()
+const Vcviewer = ref(null)
+const cesium = ref(null)
 const location = defineProps({
   lng: undefined,
   lat: undefined
@@ -37,6 +37,7 @@ watch(
     }
 )
 
+
 const onViewerReady = async ({Cesium, viewer}) => {
   if (viewerRef.value) {
 
@@ -50,13 +51,16 @@ const onViewerReady = async ({Cesium, viewer}) => {
       isViewerReady.value = true
       Vcviewer.value = viewer
       cesium.value = Cesium
-     await loadPlanes(viewer)
-
+      await loadPlanes(viewer)
+      setInterval(async () => {
+       await movePlanes(viewer)
+      }, 10000)
     } catch (error) {
       console.error("Error loading tileset:", error);
     }
   }
 };
+
 
 
 </script>
