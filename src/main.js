fd6336edaaadb710d3932885b949af48831a9794
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-20 07:25:57 +0100

    fix(): Remove cesiumPath from VueCesium configuration

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import lang from 'vue-cesium/es/locale/lang/en-us.mjs';
 const app = createApp(App)
 app.mount('#app')
 app.use(VueCesium,{
-    cesiumPath: 'https://unpkg.com/cesium@latest/Build/Cesium/Cesium.js',
     accessToken: import.meta.env.VITE_CESIUM_ACCESS_TOKEN,
     locale: lang
 })
