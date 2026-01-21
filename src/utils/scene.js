ec268649f60dc5ea1c6b240f25f9568b464c9aaa
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-21 11:56:29 +0100

    feat(): Do nothing if an eror occurred during fetch

--- src/utils/scene.js
+++ src/utils/scene.js
@@ -32,62 +32,62 @@ export async function loadPlanes(viewer){
     const airplaneUri = await Cesium.IonResource.fromAssetId(4359085);
     const data = await getFLights('http://localhost:8080/flights', {long:6.500465335539498 , lat: 46.82166054184684})
 
-    if(!data) return;
-    const osmBuildings = await Cesium.createOsmBuildingsAsync();
-    // Uncomment if you want 3d buildings dispalyed
-    // viewer.scene.primitives.add(osmBuildings);
+    if(data !== undefined) {
+        const osmBuildings = await Cesium.createOsmBuildingsAsync();
+        // Uncomment if you want 3d buildings dispalyed
+        // viewer.scene.primitives.add(osmBuildings);
 
-    /* Initialize the viewer clock:
-      Assume the radar samples are 30 seconds apart, and calculate the entire flight duration based on that assumption.
-      Get the start and stop date times of the flight, where the start is the known flight departure time (converted from PST
-        to UTC) and the stop is the start plus the calculated duration. (Note that Cesium uses Julian dates. See
-        https://simple.wikipedia.org/wiki/Julian_day.)
-      Initialize the viewer's clock by setting its start and stop to the flight start and stop times we just calculated.
-      Also, set the viewer's current time to the start time and take the user to that time.
-    */
-    const timeStepInSeconds = 30;
-    // const totalSeconds = timeStepInSeconds * (flightData.length - 1);
-    const start = Cesium.JulianDate.now();
-    const stop = Cesium.JulianDate.addSeconds(start, 10000000000, new Cesium.JulianDate());
+        /* Initialize the viewer clock:
+          Assume the radar samples are 30 seconds apart, and calculate the entire flight duration based on that assumption.
+          Get the start and stop date times of the flight, where the start is the known flight departure time (converted from PST
+            to UTC) and the stop is the start plus the calculated duration. (Note that Cesium uses Julian dates. See
+            https://simple.wikipedia.org/wiki/Julian_day.)
+          Initialize the viewer's clock by setting its start and stop to the flight start and stop times we just calculated.
+          Also, set the viewer's current time to the start time and take the user to that time.
+        */
+        const timeStepInSeconds = 30;
+        // const totalSeconds = timeStepInSeconds * (flightData.length - 1);
+        const start = Cesium.JulianDate.now();
+        const stop = Cesium.JulianDate.addSeconds(start, 10000000000, new Cesium.JulianDate());
 
-    viewer.clock.startTime = start.clone();
-    viewer.clock.stopTime = stop.clone();
-    viewer.clock.currentTime = start.clone();
+        viewer.clock.startTime = start.clone();
+        viewer.clock.stopTime = stop.clone();
+        viewer.clock.currentTime = start.clone();
 
-    // viewer.timeline.zoomTo(start, stop);
-    // Speed up the playback speed 50x.
-    viewer.clock.multiplier = 2;
-    // Start playing the scene.
-    viewer.clock.shouldAnimate = true;
+        // viewer.timeline.zoomTo(start, stop);
+        // Speed up the playback speed 50x.
+        viewer.clock.multiplier = 2;
+        // Start playing the scene.
+        viewer.clock.shouldAnimate = true;
 
-    // The SampledPositionedProperty stores the position and timestamp for each sample along the radar sample series.
+        // The SampledPositionedProperty stores the position and timestamp for each sample along the radar sample series.
 
-    for (let i = 0; i < data.length; i++) {
-        const positionProperty = new Cesium.SampledPositionProperty();
+        for (let i = 0; i < data.length; i++) {
+            const positionProperty = new Cesium.SampledPositionProperty();
 
-        const flight = data[i];
+            const flight = data[i];
 
-        // Declare the time for this individual sample and store it in a new JulianDate instance.
-        // const time = Cesium.JulianDate.addSeconds(start, i * timeStepInSeconds, new Cesium.JulianDate());
+            // Declare the time for this individual sample and store it in a new JulianDate instance.
+            // const time = Cesium.JulianDate.addSeconds(start, i * timeStepInSeconds, new Cesium.JulianDate());
 
-        const position = Cesium.Cartesian3.fromDegrees(flight.long, flight.lat, flight.alt);
-        // Store the position along with its timestamp.
-        // Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
-        positionProperty.addSample(start, position);
-        // Make planes appear even if it's too late
-        positionProperty.forwardExtrapolationType = Cesium.ExtrapolationType.HOLD
-        positionProperty.backwardExtrapolationType = Cesium.ExtrapolationType.HOLD
+            const position = Cesium.Cartesian3.fromDegrees(flight.long, flight.lat, flight.alt * 1000);
+            // Store the position along with its timestamp.
+            // Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
+            positionProperty.addSample(start, position);
+            // Make planes appear even if it's too late
+            positionProperty.forwardExtrapolationType = Cesium.ExtrapolationType.HOLD
+            positionProperty.backwardExtrapolationType = Cesium.ExtrapolationType.HOLD
 
-        viewer.entities.add({
-            description: `Location: (${flight.long}, ${flight.lat}, ${flight.alt})`,
-            position: position,
-            point: { pixelSize: 10, color: Cesium.Color.RED }
-        });
+            viewer.entities.add({
+                description: `Location: (${flight.long}, ${flight.lat}, ${flight.alt})`,
+                position: position,
+                point: {pixelSize: 10, color: Cesium.Color.RED}
+            });
 
-           await loadModel(viewer, start, stop, positionProperty, airplaneUri, flight.id);
+            await loadModel(viewer, start, stop, positionProperty, airplaneUri, flight.id);
 
+        }
     }
-
 
 
 }
@@ -112,21 +112,21 @@ async function loadModel(viewer, start, stop, positionProperty, airplaneUri, id)
 export async function movePlanes(viewer){
 
     const data = await getFLights('http://localhost:8080/flights', {long:6.500465335539498 , lat: 46.82166054184684})
-    if(!data) return;
 
-    data.forEach(flight => {
-       let entity = viewer.entities.values.find((element) => element.id === flight.id)
-        const time = Cesium.JulianDate.now();
-       const nextPos = Cesium.Cartesian3.fromDegrees(flight.long, flight.lat, flight.alt)
-        viewer.entities.add({
-            description: `Location: (${flight.long}, ${flight.lat}, ${flight.alt})`,
-            position: nextPos,
-            point: { pixelSize: 10, color: Cesium.Color.RED }
-        });
+    if (data !== undefined) {
+        data.forEach(flight => {
+            let entity = viewer.entities.values.find((element) => element.id === flight.id)
+            const time = Cesium.JulianDate.now();
+            const nextPos = Cesium.Cartesian3.fromDegrees(flight.long, flight.lat, flight.alt * 1000)
+            viewer.entities.add({
+                description: `Location: (${flight.long}, ${flight.lat}, ${flight.alt})`,
+                position: nextPos,
+                point: {pixelSize: 10, color: Cesium.Color.RED}
+            });
 
-        entity.position.addSample(time , nextPos)
-    })
-
+            entity.position.addSample(time, nextPos)
+        })
+    }
 }
 
 function addNextPostion(){
