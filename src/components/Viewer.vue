70ecd57c4d107f20e5e23d7c8c63861dbf0ae982
Joris Bailly <90181523+JorisBly@users.noreply.github.com>
2026-01-21 15:55:24 +0100

    Feature/fetch planes location (#57)
    * feat(): Fetch data from api
    
    * feat(): Get Id of the flight
    
    * fix(): Allow cross origin of the front
    
    * feat(): Create a requirements.txt
    
    * feat(): Move plane when received a new position
    
    * feat(): Call the move plane function
    
    * feat(): Get exception and status code when an error occurred
    
    * feat(): Do nothing if an eror occurred during fetch
    
    * feat(): Make run both frontend and backend with one command
    
    * feat(): Update requirments.txt
    
    * feat(): Remove dividing by 1000 for altitude
    
    * feat(): Make request every 30 seconde
    
    * fix(): rename Vcviewer -> mapViewer
    
    * fix(): Remove debugger
    
    * fix(): Remove useless stuff
    
    * fix(): Remove useless billboard

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {VcViewer} from "vue-cesium";
 import {ref, watch} from "vue";
-import {loadPlanes, prepareScene, removeMoving} from "@/utils/scene.js";
+import {loadPlanes, movePlanes, prepareScene, removeMoving} from "@/utils/scene.js";
 import Imagery from "@/components/imagery/Imagery.vue";
 import {flyTo} from "@/utils/camera.js";
 import Navigation from "@/components/navigation/Navigation.vue";
@@ -12,8 +12,8 @@ import MoonPhase from "@/components/MoonPhase.vue";
 const viewerRef = ref(null)
 const isViewerReady = ref(false)
 const cesiumToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN;
-const Vcviewer = ref()
-const cesium = ref()
+const mapViewer = ref(null)
+const cesium = ref(null)
 const location = defineProps({
   lng: undefined,
   lat: undefined
@@ -23,13 +23,13 @@ const location = defineProps({
 watch(
     [() => location.lat, () => location.lng],
     ([newLat, newLng]) => {
-      if (Vcviewer.value && newLat !== 0 && newLng !== 0) {
-        flyTo(Vcviewer.value.camera, cesium.value, newLat, newLng)
+      if (mapViewer.value && newLat !== 0 && newLng !== 0) {
+        flyTo(mapViewer.value.camera, cesium.value, newLat, newLng)
       } else {
         console.error(
             "Unable to update camera position: viewer is not ready or location is invalid.",
             {
-              viewerReady: !!Vcviewer.value,
+              viewerReady: !!mapViewer.value,
               lat: newLat,
               lng: newLng
             }
@@ -37,6 +37,7 @@ watch(
       }
     }
 )
+
 
 const onViewerReady = async ({Cesium, viewer}) => {
   if (viewerRef.value) {
@@ -48,16 +49,19 @@ const onViewerReady = async ({Cesium, viewer}) => {
       }
 
       flyTo(viewer.camera, Cesium, location.lat, location.lng)
-      isViewerReady.value = true
-      Vcviewer.value = viewer
+      mapViewer.value = viewer
       cesium.value = Cesium
-     await loadPlanes(viewer)
-
+      await loadPlanes(viewer)
+      isViewerReady.value = true
+      setInterval(async () => {
+       await movePlanes(viewer)
+      }, 30000)
     } catch (error) {
       console.error("Error loading tileset:", error);
     }
   }
 };
+
 
 
 </script>
--- src/components/primitive/Moon.vue
+++ src/components/primitive/Moon.vue
@@ -8,7 +8,8 @@ const billboard = ref(null)
 const cartographic = ref(null)
 const now = Cesium.JulianDate.now();
 const moonPos = Cesium.Simon1994PlanetaryPositions.computeMoonPositionInEarthInertialFrame(now);
-cartographic.value = Cesium.Cartographic.fromCartesian(moonPos);
+
+// cartographic.value = Cesium.Cartographic.fromCartesian(moonPos);
 
        point.value = {
         pixelSize: 28,
@@ -18,24 +19,17 @@ cartographic.value = Cesium.Cartographic.fromCartesian(moonPos);
         text: 'Moon ',
         pixelOffset: [0, 80]
       }
-      billboard.value = {
-        image: 'https://zouyaoji.top/vue-cesium/favicon.png',
-        scale: 1
-      }
 
 </script>
 <template>
 <!--  <el-row ref="viewerContainer" class="demo-viewer">-->
       <vc-entity
           ref="entity"
-
-          :position="{lng: cartographic.longitude, lat: cartographic.latitude, height: cartographic.height}"
+          :position="moonPos"
           :point="point"
           :label="label"
-
       >
         <!-- :coordinates = "{ west: 130, south: 20, east: 80, north: 25 }" -->
-<!--        <vc-graphics-rectangle :coordinates="[130, 20, 80, 25]" material="green"></vc-graphics-rectangle>-->
       </vc-entity>
 </template>
 
