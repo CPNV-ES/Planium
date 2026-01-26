950fb7557f3ecd373a083301224dcd051a16048b
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-26 10:11:06 +0100

    refactor(): Align camera controller with compass

--- src/components/CameraController.vue
+++ src/components/CameraController.vue
@@ -4,7 +4,7 @@
       min="-100"
       max="100"
       v-model.number="value"
-      class="fixed right-4 top-4 w-65 h-4 bg-gray-600 rounded-lg"
+      class="fixed right-4 top-20 w-65 h-4 bg-gray-600 rounded-lg"
   />
 </template>
 
