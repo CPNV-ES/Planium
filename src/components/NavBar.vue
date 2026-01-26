a36fed06b730ef212c5fc222dce89506966d20aa
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-26 11:33:34 +0100

    refactor(): Make navbar transparent and fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
+  <div class="relative w-full">
+  <div class="fixed h-16 inset-x-0 top-0 navbar bg-slate-100/30 shadow-sm">
     <a class="btn btn-ghost text-xl">Planium</a>
+  </div>
   </div>
 </template>
