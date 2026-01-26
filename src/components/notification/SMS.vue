8049ddcc6da523752c7750e642ab69af54bfc867
davidgalindo-git <>
2026-01-26 09:04:44 +0100

    chore:"comments"

--- src/components/notification/SMS.vue
+++ src/components/notification/SMS.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { ref } from "vue";
 
-const smsOptIn = ref(false);
-const phone = ref("");
+const smsOptIn = ref(false); // flag: is "Receive..." box checked?
+const phone = ref(""); // stores phone number
 
 </script>
 
 <template>
+  <!-- receive SMS checkbox -->
   <label class="flex items-center gap-2">
     <input
         type="checkbox"
@@ -17,6 +18,7 @@ const phone = ref("");
       <p>a plane is about to fly near the Moon</p>
     </div>
   </label>
+  <!-- phone number input shown if box checked -->
   <input
       v-if="smsOptIn"
       v-model="phone"
