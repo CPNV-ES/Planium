b93cbe6002ab7152bcdf1554aa1c6927befc5770
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-26 11:32:49 +0100

    refactor(): Remove useless w-full

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import CoordinateForm from "@/components/CoordinateForm.vue";
 import NavBar from "@/components/NavBar.vue";
 import Viewer from "@/components/Viewer.vue";
 </script>
 
 <template>
-  <main class="flex flex-col w-full items-center justify-center min-h-screen py-2">
+  <main class="flex flex-col items-center h-screen justify-center">
       <NavBar/>
     <Viewer/>
   </main>
