cc92f411e3cd4a3fcd948754a36052123a08da35
davidgalindo-git <>
2026-01-26 08:48:33 +0100

    feat:"sms checkbox template"

---
+++ src/components/notification/SMS.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <label class="flex items-center gap-2">
+    <input
+        type="checkbox"
+        v-model="smsOptIn"
+    />
+    <div>
+      <p>Receive SMS notification when</p>
+      <p>a plane is about to fly near the Moon</p>
+    </div>
+  </label>
+</template>
