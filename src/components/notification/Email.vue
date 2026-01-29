d88269cbd29b9450400b52d393d5922218207814
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-29 09:30:37 +0100

    refactor(): Extract email logic outside the component to be able to use it everywhere

--- src/components/notification/Email.vue
+++ src/components/notification/Email.vue
@@ -1,35 +1,12 @@
 <script setup>
 import { ref } from "vue";
+import {sendTestEmail} from "@/utils/mail.js";
 
 const emailCheck = ref(false); // flag: is "Receive..." box checked?
 const user_email = ref(""); // stores user's email
 
-async function sendTestEmail() {
-  if (!emailCheck.value) {
-    alert("Please check the box to receive email notifications.");
-    return;
-  }
-  if (!user_email.value) {
-    alert("Please enter a valid email address.");
-    return;
-  }
-
-  try{
-    const res = await fetch("http://localhost:8080/api/send-test-email", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({ user_email: user_email.value }),
-    });
-
-    if (!res.ok) throw new Error("Failed to send test email");
-
-    alert("Test email sent successfully.");
-  } catch (error) {
-    alert("Error sending email:" + error.message);
-  }
-}
 </script>
-
+<!-----------------------------------------This is a component to test the email sender manually-------------------------->
 <template>
   <!-- receive email checkbox -->
   <label class="flex items-center gap-2">
@@ -53,7 +30,7 @@ async function sendTestEmail() {
   <button
       class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition disabled:opacity-50"
       type="submit"
-      @click="sendTestEmail"
+      @click="async () => sendTestEmail(emailCheck, user_email)"
   >
     Send Test Email
   </button>
