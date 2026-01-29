f85ab66cf60ea24eae88b08bfd6e664d4177c2cd
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-29 09:41:08 +0100

    refactor(): Extract models in API/models

--- API/gmail.py
+++ API/gmail.py
@@ -3,11 +3,10 @@ import os
 from email.message import EmailMessage
 from fastapi import APIRouter, HTTPException, Body
 from pydantic import BaseModel
+from models import EmailRequest
 
 router = APIRouter()
 
-class EmailRequest(BaseModel):
-    user_email: str
 
 @router.post("/send-email")
 def send_email():
