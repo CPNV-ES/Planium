d7e411d9cf43170beea7d746ba7dab8e1ec4fb49
Jorisbly <joris.bailly.2001@gmail.com>
2026-01-29 19:07:26 +0100

    feat(): Add content in the params of sendEmail

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,24 +1,28 @@
-export async function sendTestEmail(emailCheck, user_email) {
-    if (!emailCheck) {
-        alert("Please check the box to receive email notifications.");
-        return;
-    }
+export async function sendEmail(user_email, content) {
     if (!user_email) {
         alert("Please enter a valid email address.");
         return;
     }
 
     try{
-        const res = await fetch("http://localhost:8080/api/send-test-email", {
+        const res = await fetch("http://localhost:8080/api/send-email", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({ user_email: user_email }),
+            body: JSON.stringify({ user_email: user_email , content: content}),
         });
 
         if (!res.ok) throw new Error("Failed to send test email");
 
-        alert("Test email sent successfully.");
+        alert("Email sent successfully.");
     } catch (error) {
         alert("Error sending email:" + error.message);
     }
+}
+
+export function saveEmail(email) {
+    localStorage.setItem("user_email", email);
+}
+
+export function getEmail() {
+    return localStorage.getItem("user_email");
 }
