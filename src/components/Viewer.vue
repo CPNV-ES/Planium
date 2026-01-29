3c58fc687c5b04ca70d220016cde4a4de7422300
Joris Bailly <90181523+JorisBly@users.noreply.github.com>
2026-01-29 13:42:56 +0100

    Hotfix/fix plane prediction (#96)
    * fix(): Detach the check if a plane is in front of the moon
    
    * fix(): Hide Moon mini viewer because of bug
    
    * fix(): Hide broken component for the moment

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,7 +1,14 @@
 <script setup>
 import {VcViewer} from "vue-cesium";
 import {nextTick, ref} from "vue";
-import {loadPlanes, updatePlanes, prepareScene, removeMoving, sendToLogFile} from "@/utils/scene.js";
+import {
+  loadPlanes,
+  updatePlanes,
+  prepareScene,
+  removeMoving,
+  sendToLogFile,
+  checkIfPlaneIsCloseToTheMoon
+} from "@/utils/scene.js";
 import Imagery from "@/components/imagery/Imagery.vue";
 import {flyTo} from "@/utils/camera.js";
 import Navigation from "@/components/navigation/Navigation.vue";
@@ -72,6 +79,10 @@ const onViewerReady = async ({Cesium, viewer}) => {
         }
       }, 59000)
 
+      setInterval(async () => {
+        checkIfPlaneIsCloseToTheMoon(mapViewer.value)
+      }, 30000)
+
     } catch (error) {
       console.error("Error loading tileset:", error);
     }
@@ -114,13 +125,13 @@ async function onLocationSubmitted(e){
           :cesium="cesium"
           :moonComponent="moonComponentRef"
       />
-      <MoonMiniViewer
-          v-if="cesium && mapViewer"
-          :mainViewer="mapViewer"
-          :Cesium="cesium"
-          :moonPos="moonComponentRef?.moonPos"
-          :planes="planeData"
-      />
+<!--      <MoonMiniViewer-->
+<!--          v-if="cesium && mapViewer"-->
+<!--          :mainViewer="mapViewer"-->
+<!--          :Cesium="cesium"-->
+<!--          :moonPos="moonComponentRef?.moonPos"-->
+<!--          :planes="planeData"-->
+<!--      />-->
       <CameraController v-if="isViewerReady" :viewer="mapViewer" />
       <CompassIndicator v-if="isViewerReady" :viewer="mapViewer" />
       <CoordinateForm @submit="onLocationSubmitted"/>
